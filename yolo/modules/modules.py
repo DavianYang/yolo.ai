47d366c250a7a1bf8a1af4626375d0adafba8409
DavianYang <kothantyarzarhein@gmail.com>
2021-03-22 18:25:06 +0630

    [Add] SEBlocks & MBConvBlock

--- yolo/modules/modules.py
+++ yolo/modules/modules.py
@@ -1,47 +1,11 @@
-from typing import Any, List
+from typing import Union, Callable, AnyStr, Any
 
+import torch
 from torch import nn
 import torch.nn.functional as F
 from torch import Tensor
 
-def make_layers(cfg: List[tuple], in_channels: int) -> nn.Sequential:
-    layers = nn.ModuleList()
-
-    for x in cfg:
-        if "Conv" in str(type(x)):
-            kernel_size, filters, stride, padding = x
-            layers += [ConvBlock(in_channels, filters, kernel_size, stride, padding)]
-
-            in_channels = filters
-
-        elif "ConvWithoutBN" in str(type(x)):
-            kernel_size, filters, stride, padding = x
-            layers += [nn.Conv2d(in_channels, filters, kernel_size, stride, padding)]
-
-            in_channels = filters
-
-        elif "MaxPool" in str(type(x)):
-            kernel_size, stride = x
-            layers += [nn.MaxPool2d(kernel_size, stride)]
-
-        elif "RepeatWithResidual" in str(type(x)):
-            for _ in range(x.nums):
-                layers += [ResBlock(in_channels, x.blocks)]
-                in_channels = x.blocks[-1].filters
-
-        elif "Repeat" in str(type(x)):
-            for _ in range(x.nums):
-                for conv in x.blocks:
-                    filters, kernel_size, stride, padding = conv
-                    layers += [ConvBlock(in_channels, conv.filters, kernel_size, stride, padding)]
-
-                    in_channels = filters
-        
-        elif "ScalePred" in str(type(x)):
-            num_classes, num_anchors = x
-            layers += [ScalePrediction(in_channels, num_classes, num_anchors)]
-
-    return nn.Sequential(*layers)
+from yolo.utils.modules import get_activation_layer
 
 class ConvBlock(nn.Module):
     def __init__(
@@ -51,17 +15,27 @@ class ConvBlock(nn.Module):
         kernel_size: int,
         stride: int = 1,
         padding: int = 0,
-        dilation: int = 1
+        dilation: int = 1,
+        groups: int = 1,
+        bias: bool = False,
+        activation: Union[Callable, AnyStr] = (lambda: nn.ReLU(inplace=True))
     ) -> None:
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.1, inplace=True)
-        )
+        self.activate = (activation is not None)
+        self.conv = nn.Conv2d(in_channels, out_channels, 
+                      kernel_size, stride, 
+                      padding, dilation,
+                      groups, bias
+                     )
+        self.bn = nn.BatchNorm2d(out_channels)
+        if self.activate:
+            self.act = get_activation_layer(activation)
         
     def forward(self, x: Tensor) -> Tensor:
-        return self.conv(x)
+        x = self.bn(self.conv(x))
+        if self.activate:
+            x = self.act(x)
+        return x
 
 
 class ResBlock(nn.Module):
@@ -74,8 +48,12 @@ class ResBlock(nn.Module):
         conv1 = blocks[0]
         conv2 = blocks[1]
         self.resblock = nn.Sequential(
-            ConvBlock(channels, conv1.filters, kernel_size=conv1.kernel_size, stride=conv1.stride, padding=conv1.padding),
-            ConvBlock(conv1.filters, conv2.filters, kernel_size=conv2.kernel_size, stride=conv2.stride, padding=conv2.padding)
+            ConvBlock(channels, conv1.filters, 
+                      kernel_size=conv1.kernel_size, 
+                      stride=conv1.stride, padding=conv1.padding),
+            ConvBlock(conv1.filters, conv2.filters, 
+                      kernel_size=conv2.kernel_size, 
+                      stride=conv2.stride, padding=conv2.padding)
         )
     
     def forward(self, x: Tensor) -> Tensor:
@@ -100,7 +78,6 @@ class Upsample(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         return self.upsample(x)
         
-
 class ScalePrediction(nn.Module):
     def __init__(
         self,
@@ -122,41 +99,81 @@ class ScalePrediction(nn.Module):
             .reshape(x.shape[0],  self.num_anchors, self.num_classes + 5, x.shape[2], x.shape[3])
             .permute(0, 1, 3, 4, 2) # N x num_anchors x 13 x 13 x 5 + num_classes
         )
-
-# Thanks to Zhenliang He for the code for SpaceToDepth
-# https://discuss.pytorch.org/t/is-there-any-layer-like-tensorflows-space-to-depth-function/3487/15   
-class SpaceToDepth(nn.Module):
-    def __init__(self, block_size):
+        
+class SEBlock(nn.Module):
+    def __init__(
+        self, 
+        in_channels: int, 
+        squeeze_channels: int, 
+        activation: Union[Callable, AnyStr] = (lambda: nn.SiLU())
+    ) -> None:
         super().__init__()
-        self.bs = block_size
-    
-    def forward(self, x):
-        N, C, H, W = x.size()
-        x = x.view(
-            N, C, H // self.bs, self.bs, W // self.bs, self.bs
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, squeeze_channels, kernel_size=1),
+            get_activation_layer(activation),
+            nn.Conv2d(squeeze_channels, in_channels, kernel_size=1),
+            nn.Sigmoid()
         )
         
-        x = x.permute(0, 3, 5, 1, 2, 4).contiguous() # (N, bs, bs, C, H//bs, W//bs)
-        x = x.view(N, C * (self.bs ** 2), H // self.bs, W // self.bs) # (N, C*bs^2, H//bs, W//bs)
-
-
-class ReorgBlock(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        return x * self.se(x)
+    
+class MBConvBlock(nn.Module):
     def __init__(
         self,
-        stride: int
-    ):
-        """ To combine middle-level features and high-level features and getter classification accuracy """
-        
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        padding: int,
+        expand_ratio: float,
+        reduction: int = 4, # squeeze excitation
+        survival_prob: float = 0.8 # for stochastic depth
+    ) -> None:
         super().__init__()
-        self.stride = stride
+        self.survival_prob = survival_prob
+        self.use_residual = in_channels == out_channels and stride == 1
         
-    def forward(self, x):
-        batch_size, channels, height, width = x.size()
-        _height, _width = height // self.stride, width // self.stride
+        hidden_dim = in_channels * expand_ratio
         
-        x = x.view(batch_size, channels, _height, self.stride, _width, self.stride).transpose(3, 4).contiguous()
-        x = x.view(batch_size, channels, _height * _width, self.stride * self.stride).transpose(2, 3).contiguous()
-        x = x.view(batch_size, channels, self.stride * self.stride, _height * _width).transpose(1, 2).contiguous()
-        x = x.view(batch_size, -1, _height, _width)
+        self.expand = in_channels != hidden_dim
+        squeeze_dim = int(in_channels / reduction)
         
+        if self.expand:
+            self.expand_conv = ConvBlock(
+                in_channels, 
+                hidden_dim, 
+                kernel_size=3, 
+                stride=1, 
+                padding=1, 
+                activation='silu'
+            )
+        self.conv = nn.Sequential(
+            ConvBlock(
+                hidden_dim, 
+                hidden_dim, 
+                kernel_size,
+                stride,
+                padding,
+                groups=hidden_dim,
+                activation='silu'
+            ),
+            SEBlock(hidden_dim, squeeze_dim),
+            nn.Conv2d(hidden_dim, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        
+    def forward(self, inputs: Tensor) -> Tensor:
+        x = self.expand_conv(inputs) if self.expand else inputs
+        x = self.stochastic_depth(self.conv(x)) + inputs if self.use_residual else self.conv(x)
         return x
+        
+        
+    def stochastic_depth(self, x: Tensor) -> Tensor:
+        if not self.training:
+            return x
+        
+        binary_tensor = torch.rand(x.shape[0], 1, 1, 1, device=x.device) < self.survival_prob
+        
+        return torch.div(x, self.survival_prob) * binary_tensor
