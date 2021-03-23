9ce25b2310db3fd26453b9bb9ac0cc12f8db592d
DavianYang <kothantyarzarhein@gmail.com>
2021-03-23 19:47:11 +0630

    [Add] Pascal VOC Dataseet

--- yolo/datasets/voc.py
+++ yolo/datasets/voc.py
@@ -1,14 +1,13 @@
-from xml.etree import ElementTree as ET
+import xml.etree.ElementTree as ET
 from typing import Callable, Optional
 
 import copy
-from PIL import Image, ImageFile
+import numpy as np
+from PIL import Image
 
 import torch
 from torchvision.datasets import VOCDetection
 from torchvision.ops.boxes import box_iou
-
-# ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 VOC_CLASSES = [
     "aeroplane",
@@ -48,14 +47,52 @@ class VOCDataset(VOCDetection):
         self.transforms = transforms
         self.anchor_boxes = torch.tensor(anchor_boxes)
         self.num_classes = num_classes
+    
+         
+    def __getitem__(self, index):
+        image = Image.open(self.images[index]).convert("RGB")
         
-    def _generate_label_matrix(self, S, targets, anchor_boxes):
+        root_ = ET.parse(self.annotations[index]).getroot()
+        targets = []
+        for obj in root_.iter("object"):
+            target = []
+            target.append(VOC_CLASSES.index(obj.find("name").text))
+            bbox = obj.find('bndbox')
+            for xyxy in ("xmin", "ymin", "xmax", "ymax"):
+                target.append(int(bbox.find(xyxy).text))
+            targets.append(target)
+        targets = torch.tensor(targets)
+        image = np.array(image)
+        if self.transforms:
+            output_labels_list = targets[:, 0].int().tolist()
+            if type(output_labels_list) == str:
+                output_labels_list = [output_labels_list]
+            transformed_items = self.transforms(
+                image=image, bboxes=targets[:, 1:], class_labels=output_labels_list
+            )
+            image = transformed_items["image"]
+            boxes = transformed_items["bboxes"]
+            class_labels = transformed_items["class_labels"]
+        
+        small_label_matrix = self._generate_label_matrix(
+            13, boxes, class_labels, copy.deepcopy(self.anchor_boxes)[2] / (416 / 13)
+        )
+        medium_label_matrix = self._generate_label_matrix(
+            26, boxes, class_labels, copy.deepcopy(self.anchor_boxes)[1] / (416 / 26)
+        )
+        large_label_matrix = self._generate_label_matrix(
+            52, boxes, class_labels, copy.deepcopy(self.anchor_boxes)[0] / (416 / 52)
+        )
+        return image, (small_label_matrix, medium_label_matrix, large_label_matrix)
+    
+    
+    def _generate_label_matrix(self, S, boxes, class_labels, anchor_boxes):
         label_matrix = torch.zeros(
             (S, S, len(anchor_boxes), 5 + self.num_classes), dtype=torch.float64
         )
 
-        for target in targets:
-            class_label, xmin, ymin, xmax, ymax = target
+        for box, class_label in zip(boxes, class_labels):
+            xmin, ymin, xmax, ymax = box
             
             x = xmin
             y = ymin
@@ -68,7 +105,8 @@ class VOCDataset(VOCDetection):
             h = (ymax - ymin) / 416
             
             i, j = int(S * x), int(S * y)
-            x_cell, y_cell = S * x, S * y
+            
+            x_cell, y_cell = S * x - i, S * y - j
             # We need iou of anchor box and bbox as if they have same xmin and ymin, as only the width and height matters while assigning the bbox to anchor box.
             anchor_boxes[:, 0] = xmin
             anchor_boxes[:, 1] = ymin
@@ -92,33 +130,5 @@ class VOCDataset(VOCDetection):
             label_matrix[j, i, max_idx[0], 4] = 1
             # set one hot coding for class label
             label_matrix[j, i, max_idx[0], 5 + class_label] = 1
+        
         return label_matrix
-            
-             
-    def __getitem__(self, index):
-        img = Image.open(self.images[index]).convert("RGB")
-        
-        root_ = ET.parse(self.annotations[index]).getroot()
-        targets = []
-        for obj in root_.iter("object"):
-            target = []
-            target.append(VOC_CLASSES.index(obj.find("name").text))
-            bbox = obj.find('bndbox')
-            for xyxy in ("xmin", "ymin", "xmax", "ymax"):
-                target.append(int(bbox.find(xyxy).text))
-            targets.append(target)
-        targets = torch.tensor(targets)
-        
-        if self.transforms:
-            pass
-        
-        small_label_matrix = self._generate_label_matrix(
-            13, targets, copy.deepcopy(self.anchor_boxes)[6:9] / (416 / 13)
-        )
-        medium_label_matrix = self._generate_label_matrix(
-            26, targets, copy.deepcopy(self.anchor_boxes)[3:6] / (416 / 26)
-        )
-        large_label_matrix = self._generate_label_matrix(
-            52, targets, copy.deepcopy(self.anchor_boxes)[:3] / (416 / 52)
-        )
-        return img, (small_label_matrix, medium_label_matrix, large_label_matrix)
