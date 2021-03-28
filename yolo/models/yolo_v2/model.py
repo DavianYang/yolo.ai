9bce2c7c7238b4569b9ca59d420f69eea29e8cdc
DavianYang <kothantyarzarhein@gmail.com>
2021-03-28 21:57:42 +0630

    [Add] YOLOv2 Model

--- yolo/models/yolo_v2/model.py
+++ yolo/models/yolo_v2/model.py
@@ -8,31 +8,30 @@ from yolo.modules.modules import ConvBlock, SpaceToDepth
 class YOLOv2(nn.Module):
     def __init__(
         self,
-        anchor_boxes: list,
         grid_size: int = 13,
+        num_anchors: int = 5,
         num_classes: int = 20,
     ) -> None:
         super().__init__()
         self.grid_size = grid_size
         self.num_classes = num_classes
-        self.num_anchors = len(anchor_boxes)
-        self.anchor_boxes = torch.tensor(anchor_boxes)
+        self.num_anchors = num_anchors
         
         # Detection Head
-        self.darknet_head = darknet19(mode="head")
+        self.backbone_head = darknet19(mode="head")
         self.middle = ConvBlock(512, 64, kernel_size=1, stride=1)
         self.space_to_depth = SpaceToDepth(block_size=2)
-        self.darknet_tail = darknet19(mode="tail")
+        self.backbone_tail = darknet19(mode="tail")
         
         self.conv = ConvBlock(1280, 1024, kernel_size=3, stride=1, padding=1)
-        self.pred = nn.Conv2d(1024, self.anchor_boxes * (4 + 1 + num_classes), 
+        self.pred = nn.Conv2d(1024, self.num_anchors * (4 + 1 + self.num_classes), 
                                kernel_size=1, stride=1, bias=True)
         
     def forward(self, x: Tensor) -> Tensor:
-        x = self.darknet_head(x)
+        x = self.backbone_head(x)
         middle = self.middle(x)
         middle = self.space_to_depth(middle)
-        x = self.darknet_tail(x)
+        x = self.backbone_tail(x)
         x = torch.cat([middle, x], dim=1)
         
         x = self.conv(x)
