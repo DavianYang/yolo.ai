9631333864fe42f50e7fc4e721cbe475046f5e51
DavianYang <kothantyarzarhein@gmail.com>
2021-04-20 22:00:35 +0630

    [Refactor] workflows

--- scripts/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xu
-
-if [[ "${TORCH_UTILS_COLLECT_ENV}" -eq 1 ]]; then
-    python -m torch.utils.collect_env
-fi
-
-coverage run --source package -m unittest discover -bv -s ./tests/ -p "test_*.py" && coverage report
