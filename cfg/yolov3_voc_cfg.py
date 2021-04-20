3ebc21d1411e024d39e9db812824c440c3a86740
DavianYang <kothantyarzarhein@gmail.com>
2021-04-20 21:57:52 +0630

    [Refactor] Anchors

--- cfg/yolov3_voc_cfg.py
+++ cfg/yolov3_voc_cfg.py
@@ -25,6 +25,12 @@ training_pipeline = []
 testing_pipeline = []
 val_pipeline = []
 
+yolov3_anchors = [
+    [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
+    [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
+    [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
+]
+
 classes = [
     "aeroplane",
     "bicycle",
