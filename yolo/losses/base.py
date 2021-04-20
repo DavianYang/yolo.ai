f26d7a1e3e324271dbb9ce58d78cf21e376549b0
DavianYang <kothantyarzarhein@gmail.com>
2021-04-20 21:56:16 +0630

    [Refactor] Yolov2 and Yolov3 loss

--- yolo/losses/base.py
+++ yolo/losses/base.py
@@ -8,7 +8,7 @@ from yolo.metrics.functional import IOU
 class YOLOLoss(nn.Module):
     def __init__(self, anchors_boxes: List[Tuple[float]]):
         super().__init__()
-        self.anchor_boxes = anchors_boxes
+        self.anchor_boxes = anchors_boxes.reshape(1, 3, 1, 1, 2)
         
         self.mse = nn.MSELoss()
         self.bce = nn.BCEWithLogitsLoss()
@@ -21,49 +21,35 @@ class YOLOLoss(nn.Module):
         self.lambda_obj = 1
         self.lambda_box = 10
 
-    def forward(self, predictions, target):
-        obj = target[..., 0] == 1  # in paper this is Iobj_i
-        noobj = target[..., 0] == 0  # in paper this is Inoobj_i
-
-        # ======================= #
-        #   FOR NO OBJECT LOSS    #
-        # ======================= #
-
-        no_object_loss = self.bce(
-            (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
-        )
-
-        # ==================== #
-        #   FOR OBJECT LOSS    #
-        # ==================== #
-
-        anchors = self.anchor_boxes.reshape(1, 3, 1, 1, 2)
-        box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors], dim=-1)
-        ious = IOU(box_preds[obj], target[..., 1:5][obj]).detach()
-        object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
-
-        # ======================== #
-        #   FOR BOX COORDINATES    #
-        # ======================== #
-
-        predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])  # x,y coordinates
-        target[..., 3:5] = torch.log(
-            (1e-16 + target[..., 3:5] / anchors)
-        )  # width, height coordinates
-        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
-
-        # ================== #
-        #   FOR CLASS LOSS   #
-        # ================== #
-
-        class_loss = self.entropy(
-            (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
-        )
-
+    def forward(self, pred, target):
+        obj = target[..., 0] == 1
+        noobj = target[..., 0] == 0
 
         return (
-            self.lambda_box * box_loss
-            + self.lambda_obj * object_loss
-            + self.lambda_noobj * no_object_loss
-            + self.lambda_class * class_loss
+            self.lambda_obj * self._obj_loss_fn(pred, target, obj)
+            + self.lambda_noobj * self._no_obj_loss_fn(pred, target, noobj)
+            + self.lambda_box * self._box_coord_loss_fn(pred, target, obj)
+            + self.lambda_class * self._class_loss_fn(pred, target, obj)
+        )
+        
+    def _no_obj_loss_fn(self, pred, target, noobj):
+        return self.bce(
+            (pred[..., 0:1][noobj]), (target[..., 0:1][noobj]),
+        )
+    
+    def _obj_loss_fn(self, pred, target, obj):
+        box_preds = torch.cat([self.sigmoid(pred[..., 1:3]), torch.exp(pred[..., 3:5]) * self.anchor_boxes], dim=-1)
+        ious = IOU(box_preds[obj], target[..., 1:5][obj]).detach()
+        return self.mse(self.sigmoid(pred[..., 0:1][obj]), ious * target[..., 0:1][obj])
+    
+    def _box_coord_loss_fn(self, pred, target, obj):
+        pred[..., 1:3] = self.sigmoid(pred[..., 1:3])  # x,y coordinates
+        target[..., 3:5] = torch.log(
+            (1e-16 + target[..., 3:5] / self.anchor_boxes)
+        )  # width, height coordinates
+        return self.mse(pred[..., 1:5][obj], target[..., 1:5][obj])
+    
+    def _class_loss_fn(self, pred, target, obj):
+        return self.entropy(
+            (pred[..., 5:][obj]), (target[..., 5][obj].long()),
         )
